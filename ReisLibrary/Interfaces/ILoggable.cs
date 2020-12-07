7ad033bc2cc20066825c59bc913b38cdee6e8dd9
YANNICK BETERAMS <YANNICK.BETERAMS@student.intecbrussel.be>
2020-12-07 23:24:25 +0100

    update

--- ReisLibrary/Interfaces/ILoggable.cs
+++ ReisLibrary/Interfaces/ILoggable.cs
@@ -1,9 +1,4 @@
 ﻿using ReisLibrary.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReisLibrary.Interfaces
 {
